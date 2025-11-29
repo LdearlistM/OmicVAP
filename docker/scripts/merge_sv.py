73ac1108f86efb6fa497bf1880dea05de4bace07
LdearlistM <100986579+LdearlistM@users.noreply.github.com>
2025-11-29 21:58:56 +0800

    Refactor merge_sv.py to clean up warnings and comments
    Removed commented-out code and adjusted warnings handling.

--- docker/scripts/merge_sv.py
+++ docker/scripts/merge_sv.py
@@ -6,8 +6,7 @@ from snakemake.script import snakemake
 from SGVFinder2 import work_on_collection
 import pandas as pd
 import logging # Import the logging library
-import warnings  # 1. 导入 warnings 模块
-
+import warnings
 
 # --- Configure Logging ---
 # Get log file path from Snakemake
@@ -40,10 +39,8 @@ try: # Wrap everything in a try...except for robustness
     logging.info(f"Running work_on_collection on {samp_to_map}")
     
     with warnings.catch_warnings():
-        # 3. 在这里设置过滤器，忽略特定类型的警告
         warnings.simplefilter("ignore", category=FutureWarning)
         warnings.simplefilter("ignore", category=RuntimeWarning)
-        # warnings.filterwarnings("ignore", message="Mean of empty slice") # 也可以按消息内容过滤
         vsgv, dsgv = work_on_collection(
             samp_to_map=samp_to_map,
             max_spacing=10,
@@ -104,13 +101,6 @@ try: # Wrap everything in a try...except for robustness
                     # Retain .df file for debugging
         logging.info(f"Finished processing .df files. Converted: {processed_count}, Errors: {error_count}")
 
-    # if os.path.isdir(samp_to_map):
-    #     logging.info(f"Removing temporary directory: {samp_to_map}")
-    #     try:
-    #         shutil.rmtree(samp_to_map)
-    #         logging.info(f"Successfully removed {samp_to_map}")
-    #     except OSError as e:
-    #         logging.warning(f"Warning: Could not delete {samp_to_map}: {e}")
 except Exception as e:
     logging.exception("An unexpected error occurred during script execution.")
     sys.exit(1)
